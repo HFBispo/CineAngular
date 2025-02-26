d75cf12cf7c67096d6814acf5fbefc2c9a942a76
Henrique Bispo <henrique.bispo@simples.software>
2025-02-26 09:22:40 -0300

    :sparkles: feat: Adicionando temas dark e light

--- src/app/pages/configurations/configurations.component.ts
+++ src/app/pages/configurations/configurations.component.ts
@@ -1,12 +1,11 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { LanguageSelectorComponent } from '../../components/language-selector/language-selector.component';
 import { TranslatePipe } from '@ngx-translate/core';
-import { ThemeSelectorComponent } from "../../components/theme-selector/theme-selector.component";
 import { BreadcrumbService } from '../../services/breadcrumb.service';
 
 @Component({
   selector: 'app-configurations',
-  imports: [LanguageSelectorComponent, TranslatePipe, ThemeSelectorComponent],
+  imports: [LanguageSelectorComponent, TranslatePipe],
   templateUrl: './configurations.component.html',
   styleUrl: './configurations.component.scss'
 })
